01dd73253dafaa2341c3425fe0cfa02bcdf4102b
chandan2812 <chandanpms@gmail.com>
2024-12-09 13:41:14 +0530

    added scroll to top button

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -6,10 +6,34 @@ import Universities from "../components/Universities";
 import Features from "../components/Features";
 import AboutUs from "../components/AboutUs";
 import StatsSection from "../components/Stats";
+import { useEffect, useState } from "react";
 
 
 
 function Landing() {
+    const [isVisible, setIsVisible] = useState(false);
+    
+  // Handle scroll to top button visibility
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
     return (
         <div id="home">
             <Nav/>
@@ -20,6 +44,14 @@ function Landing() {
             <AboutUs/>
             <FAQ/>
             <Footer/>
+            {isVisible && (
+        <button
+          onClick={scrollToTop}
+          className="z-20 fixed bottom-10 right-10 bg-blue-600 text-white w-12 h-12 rounded-full shadow-lg hover:bg-blue-700 transition-all duration-300 ease-in-out transform hover:scale-110 flex items-center justify-center"
+        >
+          ↑
+        </button>
+      )}
         </div>
     );
 }
